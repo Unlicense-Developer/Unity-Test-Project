072117e202406420ae4930f08a0aeeb9bba0d90b
HyunJun Sung <shj9866@gmail.com>
2023-10-25 21:27:39 +0900

    10/25 Unity Base Test
    10/25 Unity Base Test

---
+++ Unity Test Project/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float moveSpeed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+
+    }
+}
