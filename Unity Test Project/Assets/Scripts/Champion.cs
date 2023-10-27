b092bbafbabeae218bb26d366abfde7556762663
HyungsukChoi <shj9866@gmail.com>
2023-10-27 18:21:09 +0900

    10/27 Move & Player Script

---
+++ Unity Test Project/Assets/Scripts/Champion.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Champion : MonoBehaviour
+{
+    struct Stat
+    {
+        string name;
+        int ad;
+        int ap;
+        int armor;
+        int resist;
+        float atkSpeed;
+        float abilHaste;
+        float criticalChance;
+        int criticalDamage;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Unity Test Project/Assets/Scripts/Player.cs
+++ Unity Test Project/Assets/Scripts/Player.cs
@@ -2,25 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Champion
 {
-    float moveSpeed = 5.0f;
-    Vector3 direction;
+    Vector3 movePos;
+    float moveSpeed = 3.4f;
+    bool isMove = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        PickingPos();
         Move();
+    }
+
+    void PickingPos()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                movePos = hit.point;
+                isMove = true;
+            }
+        }
     }
 
     void Move()
     {
-        
+        if (!isMove) return;
+
+        transform.position = Vector3.MoveTowards(transform.position, movePos, Time.deltaTime * moveSpeed);
+
+        Quaternion direction = Quaternion.LookRotation(movePos - transform.position);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, direction, Time.deltaTime * 900.0f);
+
+        if (movePos == transform.position)
+            isMove = false;
     }
 }
